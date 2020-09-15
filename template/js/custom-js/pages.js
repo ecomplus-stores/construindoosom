3f5522b2d2b9eec2ee318e76fe8be461bf36afaf
Matheus Reis <35343551+matheusgnreis@users.noreply.github.com>
2020-09-15 11:18:33 -0300

    Update pages.js

--- template/js/custom-js/pages.js
+++ template/js/custom-js/pages.js
@@ -1,1 +1,6 @@
 // Add your custom JavaScript for storefront pages here.
+if (window.innerWidth < 991) {
+  const menuList = document.querySelectorAll('.menu__list a')
+  const position = menuList.length
+  menuList[position - 1].after('<a href="https://loja.construindoosom.com.br/pages/sobre-nos" class="menu__item"><span>Sobre nós</span></a>')
+}
