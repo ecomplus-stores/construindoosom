7cd1a671d997efd69d5751347b01f4d77d672939
Matheus Reis <35343551+matheusgnreis@users.noreply.github.com>
2020-09-15 11:25:50 -0300

    Update pages.js

--- template/js/custom-js/pages.js
+++ template/js/custom-js/pages.js
@@ -2,5 +2,6 @@
 if (window.innerWidth < 991) {
   const menuList = document.querySelectorAll('.menu__list a')
   const position = menuList.length
-  menuList[position - 1].after('<a href="https://loja.construindoosom.com.br/pages/sobre-nos" class="menu__item"><span>Sobre nós</span></a>')
+  const newList = '<a href="https://loja.construindoosom.com.br/pages/sobre-nos" class="menu__item"><span>Sobre nós</span></a>'
+  menuList[position - 1].after(newList)
 }
