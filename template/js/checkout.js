f9f1a2f812c2febf9aeaf4c58f55824c5ab3c0d0
Matheus Reis <35343551+matheusgnreis@users.noreply.github.com>
2021-05-05 14:39:58 -0300

    chore: update layout with confirm

--- template/js/checkout.js
+++ template/js/checkout.js
@@ -1,2 +1,18 @@
 import '#template/js/checkout'
 import './custom-js/checkout'
+
+const routeArr = window.location.href.split('/')
+console.log(routeArr);
+const route = routeArr[routeArr.length - 1]
+const route2 = routeArr[routeArr.length - 2]
+const route3 = routeArr[routeArr.length - 3]
+console.log(route)
+if(route == 'checkout') {
+  document.getElementById('checkToPay').style.display = 'none'
+}
+if(route2 == 'account') {
+    document.getElementById('checkToPay').style.display = 'none'
+}
+if(route3 == 'order') {
+    document.getElementById('checkToPay').style.display = 'none'
+}
